ee801f2baddfb31bdb9932e06ea65efc12f1845d
Christian Praiß <ChristianPraiss@users.noreply.github.com>
2018-09-12 16:11:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const React = require('react');
-import PropTypes from 'prop-types';
 const ReactNative = require('react-native');
 const {
     View,
@@ -10,17 +9,6 @@ const {
 
 const MarqueeLabel = React.createClass({
     propTypes: {
-        ...View.propTypes,
-        text : PropTypes.string.isRequired,
-        scrollDuration : PropTypes.number, // 秒
-        marqueeType : PropTypes.number, // ios
-        fadeLength : PropTypes.number, // ios
-        leadingBuffer : PropTypes.number, // ios
-        trailingBuffer : PropTypes.number, // ios
-        animationDelay : PropTypes.number, // ios
-        isRepeat : PropTypes.bool, // android
-        startPoint : PropTypes.number, // android
-        direction : PropTypes.number, //android
     },
     render: function () {
         const { children, ...props } = this.props;
